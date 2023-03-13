4b31ee03ba866972d854000fda2e657f45fe0d68
apaul30 <anshulpaul@berkeley.edu>
2023-03-13 05:19:43 -0700

    Configs env file

---
+++ configs.py
@@ -0,0 +1,24 @@
+import boto3
+import openai 
+import botocore
+import os
+from env import *
+from botocore.exceptions import ClientError
+
+# OPEN AI
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+
+# WASABI
+wasabi = boto3.resource('s3',
+    endpoint_url='https://s3.us-west-1.wasabisys.com',
+    aws_access_key_id=os.environ.get('WASABI_AWS_KEY'),
+    aws_secret_access_key=os.environ.get('WASABI_AWS_SECRET_KEY')
+)
+
+# LOCAL DB
+db_host  =  "localhost" 
+db_username = "postgres"
+db_password = os.environ.get('LOCAL_DB_PASSWORD')
+db_name = "postgres"
+mail_password = os.environ.get('MAIL_PASSWORD')
