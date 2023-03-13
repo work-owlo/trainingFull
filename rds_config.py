a039c47c9f070234a4d678b1a5df4717ba893f9f
apaul30 <anshulpaul@berkeley.edu>
2023-03-12 21:16:06 -0700

    fix configs

--- rds_config.py
+++ rds_config.py
@@ -1,14 +1,14 @@
 import os
-# db_host  =  "ec2-52-21-136-176.compute-1.amazonaws.com"
-# db_username = "lzachwctznglcz"
-# db_password = "1946c315c9a28341e5cf4d7a8f2813485da6791217d4dfaaa645b4ccd8ba1190"
-# db_name = "db71bf67e7ass4"
+db_host  =  "ec2-52-21-136-176.compute-1.amazonaws.com"
+db_username = "lzachwctznglcz"
+db_password = "1946c315c9a28341e5cf4d7a8f2813485da6791217d4dfaaa645b4ccd8ba1190"
+db_name = "db71bf67e7ass4"
 
 # TEST
-db_host  =  "localhost" 
-db_username = "postgres"
-db_password = "Plane2017"
-db_name = "postgres"
+# db_host  =  "localhost" 
+# db_username = "postgres"
+# db_password = "Plane2017"
+# db_name = "postgres"
 mail_password = "Owlo0020!"
 
 # db_info = os.environ.get('DATABASE_URL')
